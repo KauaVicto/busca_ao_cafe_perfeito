82bb3c19a5524c26bcbb21fab35a9bf2173cd66d
KauaVicto <kauavictor259@gmail.com>
2024-03-01 12:02:41 -0300

    adicionado coluna de quantidade de avaliações no datatable

--- api/src/application/useCase/cafe/findAllCafe/findAllCafeController.ts
+++ api/src/application/useCase/cafe/findAllCafe/findAllCafeController.ts
@@ -14,7 +14,8 @@ export class FindAllCafeController extends Prisma {
         const cafes = await this.prisma.$queryRaw`
             SELECT 
             ca.*,
-            avg(ava.nota_geral) AS nota_geral
+            avg(ava.nota_geral) AS nota_geral,
+            count(ava.id)::Int AS total_avaliacao
             FROM cafe.tbcafes ca
             LEFT JOIN cafe.tbavaliacoes ava ON ca.id = ava.cafe_id
             GROUP BY ca.id
