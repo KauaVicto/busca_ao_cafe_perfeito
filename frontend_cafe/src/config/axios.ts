91670ae753516f313d32dee5a1d935715eca4090
KauaVicto <kauavictor259@gmail.com>
2024-05-14 22:08:57 -0300

    passado para avaliacao a criacao da avaliacao e feito retornar as informacoes de cafe

--- frontend_cafe/src/config/axios.ts
+++ frontend_cafe/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Definindo a URL base da API
-const API_URL = 'http://192.168.100.38:3001';
+const API_URL = 'http://192.168.0.151:3001';
 
 
 // Criando uma instância do Axios com a URL base configurada
